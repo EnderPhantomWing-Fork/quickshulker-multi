14e98b615d0509a7c14ecacc85fd79c4d3e8f96e
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-01-02 22:02:27 +0800

    chore: added support for 1.21.11, updated header comments for .java chore: 添加对1.21.11的支持，更新*.java的头部注释

--- src/main/java/net/kyrptonaught/quickshulker/QuickShulkerMod.java
+++ src/main/java/net/kyrptonaught/quickshulker/QuickShulkerMod.java
@@ -1,3 +1,13 @@
+/*
+ * This file is part of the Quick Shulker Multi project, licensed under the MIT License.
+ *
+ * Copyright (C) 2019 kyrptonaught, Hao_cen, Grayer0113, MoRanpcy, EnderPhantomWing and other contributors
+ *
+ * {name} is free software: you can redistribute or modify it under the terms of the MIT License.
+ *
+ * Browse the MIT License here. <https://mit-license.org/>
+ */
+
 package net.kyrptonaught.quickshulker;
 
 import net.fabricmc.api.ModInitializer;
