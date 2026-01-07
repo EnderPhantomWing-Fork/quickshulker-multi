97f18f1f431927a8bcb39e7a439d4dfb4c523bf9
EnderPhantomWing <EnderPhantomWing@outlook.com>
2026-01-07 13:31:33 +0800

    chore: fix mixin GameOptions$VisitorMixin chore: mixin修复 GameOptions$VisitorMixin

--- src/main/java/net/kyrptonaught/kyrptconfig/mixin/displaykeybind/GameOptions$VisitorMixin.java
+++ src/main/java/net/kyrptonaught/kyrptconfig/mixin/displaykeybind/GameOptions$VisitorMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(targets = "net.minecraft.client.Options$3")
 public class GameOptions$VisitorMixin {
 
-    @Inject(method = "visitString", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "process(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", at = @At("HEAD"), cancellable = true)
     public void dontDoThat(String key, String current, CallbackInfoReturnable<String> cir) {
         if (SpoofedKeysHelper.spoofed_Keys.contains(key))
             cir.setReturnValue(current);
